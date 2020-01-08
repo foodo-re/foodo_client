487081b2768c02bf097c7c977001e3b177b8ad82
최방실 <qkdtlf89089@gmail.com>
2020-01-08 17:25:29 +0900

    :sparkles: 로그인 화면 구성 및 라우터 기본 구조

--- src/page/Dashboard.js
+++ src/page/Dashboard.js
@@ -1,18 +1,19 @@
 import React, { Component } from "react";
-import ColdSection from "../component/ColdSection";
-import FrozenSection from "../component/FrozenSection";
-import ShoppingNotes from "../component/ShoppingNotes";
-import DayMenu from "../component/DayMenu";
-import Chart from "../component/Chart";
+// import ColdSection from "../component/ColdSection";
+// import FrozenSection from "../component/FrozenSection";
+// import ShoppingNotes from "../component/ShoppingNotes";
+// import DayMenu from "../component/DayMenu";
+// import Chart from "../component/Chart";
 
 function Dashboard() {
   return (
     <div>
-      <ColdSection />
+      대시보드^.^7
+      {/* <ColdSection />
       <FrozenSection />
       <ShoppingNotes />
       <DayMenu />
-      <Chart />
+      <Chart /> */}
     </div>
   );
 }
