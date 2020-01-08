09b18e489437ca6e4649a8b0561ffeea213d14a7
최방실 <qkdtlf89089@gmail.com>
2020-01-08 18:45:33 +0900

    :wrench: 컴포넌트 폴더 재생성

---
+++ src/component/Chart.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Chart = () => {
+  return <div className="chart-container"> 얘는 차트다 !</div>;
+};
+
+export default Chart;
---
+++ src/component/FrozenSection.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FrozenSection = () => {
+  return <div className="frozen-container"> 냉동! </div>;
+};
+
+export default FrozenSection;
