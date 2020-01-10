d426ba04dcdec538c246c1de683e34979830bac3
최방실 <qkdtlf89089@gmail.com>
2020-01-10 23:05:04 +0900

    대시보드 진입 시 재고 데이터 불러오기

--- src/page/Dashboard.js
+++ src/page/Dashboard.js
@@ -1,4 +1,7 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
+import { connect } from "react-redux";
+import { getStock } from "../redux/modules/stock";
+import Store from "../redux/configStore";
 import ColdSection from "../component/ColdSection";
 import FrozenSection from "../component/FrozenSection";
 import ShoppingNotes from "../component/ShoppingNotes";
@@ -7,11 +10,20 @@ import Chart from "../component/Chart";
 import "./style.css";
 
 function Dashboard() {
+  const [stock, setStock] = useState([]);
+  useEffect(() => {
+    Store.dispatch(getStock()).then(() => {
+      return Store.getState().stock.data;
+    });
+  });
+
   return (
     <div className="dash-container">
       <div className="dash-left">
-        <ColdSection />
-        <FrozenSection />
+        <div className="dash-refrg">
+          <ColdSection />
+          <FrozenSection />
+        </div>
         <ShoppingNotes />
       </div>
       <div className="dash-right">
@@ -22,4 +34,9 @@ function Dashboard() {
   );
 }
 
-export default Dashboard;
+function mapDispatchToProps(dispatch) {
+  return {
+    getStock: () => dispatch(getStock())
+  };
+}
+export default connect(null, mapDispatchToProps)(Dashboard);
