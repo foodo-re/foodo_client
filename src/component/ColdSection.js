1b8a51783fbcd9a61991d6f7dd4a9a5d9a9c420e
최방실 <qkdtlf89089@gmail.com>
2020-01-10 23:11:28 +0900

    ::construction: 대시보드 냉장고/냉동실 섹션 재고데이터 불러오기

--- src/component/ColdSection.js
+++ src/component/ColdSection.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import Store from "../redux/configStore";
 
 const ColdSection = () => {
-  return <div className="cold-container"> 냉장고 미리보기! </div>;
+  const [stock, setStock] = useState([]);
+  Store.subscribe(() => {
+    setStock(Store.getState().stock.data);
+  });
+  return (
+    <div className="cold-container">
+      *냉장고*
+      {!stock.length
+        ? null
+        : stock.map(ing => {
+            if (!ing.frozen) {
+              return (
+                <div key={ing.id}>
+                  {ing.name} / {ing.msg}
+                </div>
+              );
+            }
+          })}
+    </div>
+  );
 };
-
 export default ColdSection;
